58883d404973b38c8fb8505d6336d75e4cdcc27b
Utkarsh Badoni <utkarshbadoni2000@gmail.com>
2023-05-19 20:39:46 +0530

    Integrating Redux with react

--- redux-demo.js
+++ redux-demo.js
@@ -4,12 +4,12 @@ const redux= require('redux');
 const counterReducer=(state={counter:0 },action)=>{
    if(action.type==='increment'){
     return{
-        counter:state.counter+1
+        counter:state.counter+2
     }
 }
 if(action.type==='decrement'){
      return{
-        counter:state.counter-1
+        counter:state.counter-2
     }
 }
 }
